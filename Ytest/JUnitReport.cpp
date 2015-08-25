f2b143cda28228a810cd418eb35c649dadfec911
Jan Erik Breimo <jebreimo@gmail.com>
2015-08-25 09:18:55 +0200

    Re-enabled JUnit test-reports.

--- Ytest/JUnitReport.cpp
+++ Ytest/JUnitReport.cpp
@@ -8,15 +8,12 @@
 #include "JUnitReport.hpp"
 
 #include <iomanip>
-#include <iostream>
-// #include "JEBIO/Xml/XmlWriter.hpp"
+#include "XmlWriter.hpp"
 
 #include "Session.hpp"
 #include "Test.hpp"
 
 namespace Ytest {
-
-// using namespace Ytest_JEBIO::Xml;
 
 void addTestCases(std::map<std::string, std::vector<TestPtr>>& testCases,
                   std::string path, TestPtr test)
@@ -43,60 +40,60 @@ std::map<std::string, std::vector<TestPtr>> getTestCases(
     return testCases;
 }
 
-// void writeXmlError(XmlWriter& writer, const Error& error)
-// {
-//     if (error.type() == Error::UnhandledException)
-//         writer.beginElement("error");
-//     else
-//         writer.beginElement("failure");
-//     writer.attribute("message", error.message());
-//     writer.attribute("type", Error::levelName(error.type()));
-//     writer.characterData(error.file());
-//     writer.characterData("[");
-//     writer.characterData((int)error.lineNo());
-//     writer.characterData("]");
-//     auto& context = error.context();
-//     for (auto c = begin(context); c != end(context); ++c)
-//     {
-//         writer.characterData("\n");
-//         writer.characterData(c->text());
-//     }
-//     writer.endElement();
-// }
+ void writeXmlError(XmlWriter& writer, const Error& error)
+ {
+     if (error.type() == Error::UnhandledException)
+         writer.beginElement("error");
+     else
+         writer.beginElement("failure");
+     writer.attribute("message", error.message());
+     writer.attribute("type", Error::levelName(error.type()));
+     writer.characterData(error.file());
+     writer.characterData("[");
+     writer.characterData((int)error.lineNo());
+     writer.characterData("]");
+     auto& context = error.context();
+     for (auto c = begin(context); c != end(context); ++c)
+     {
+         writer.characterData("\n");
+         writer.characterData(c->text());
+     }
+     writer.endElement();
+ }
 
-// void writeXmlTestCase(XmlWriter& writer, const Test& test)
-// {
-//     writer.beginElement("testcase");
-//     writer.attribute("name", test.name());
-//     if (test.assertions() != 0)
-//     {
-//         writer.attribute("assertions", (int64_t)test.assertions());
-//         writer.attribute("name", "NONE");
-//         writer.attribute("time", test.elapsedTime());
-//         auto& errors = test.errors();
-//         for (auto it = begin(errors); it != end(errors); ++it)
-//             writeXmlError(writer, *it);
-//     }
-//     writer.endElement();
-// }
+ void writeXmlTestCase(XmlWriter& writer, const Test& test)
+ {
+     writer.beginElement("testcase");
+     writer.attribute("name", test.name());
+     if (test.assertions() != 0)
+     {
+         writer.attribute("assertions", (int64_t)test.assertions());
+         writer.attribute("name", "NONE");
+         writer.attribute("time", test.elapsedTime());
+         auto& errors = test.errors();
+         for (auto it = begin(errors); it != end(errors); ++it)
+             writeXmlError(writer, *it);
+     }
+     writer.endElement();
+ }
 
 void writeJUnitReport(std::ostream& os, const Session& session)
 {
-    // auto testCases = getTestCases(session.tests());
-    // XmlWriter writer(os);
-    // writer.setFormatting(XmlWriter::IndentElements);
-    // writer.beginElement("testsuites");
-    // for (auto it = begin(testCases); it != end(testCases); ++it)
-    // {
-    //     writer.beginElement("testsuite");
-    //     writer.attribute("name", it->first);
-    //     for (auto t = begin(it->second); t != end(it->second); ++t)
-    //     {
-    //         writeXmlTestCase(writer, **t);
-    //     }
-    //     writer.endElement();
-    // }
-    // writer.endElement();
+     auto testCases = getTestCases(session.tests());
+     XmlWriter writer(os);
+     writer.setFormatting(XmlWriter::IndentElements);
+     writer.beginElement("testsuites");
+     for (auto it = begin(testCases); it != end(testCases); ++it)
+     {
+         writer.beginElement("testsuite");
+         writer.attribute("name", it->first);
+         for (auto t = begin(it->second); t != end(it->second); ++t)
+         {
+             writeXmlTestCase(writer, **t);
+         }
+         writer.endElement();
+     }
+     writer.endElement();
 }
 
 }
